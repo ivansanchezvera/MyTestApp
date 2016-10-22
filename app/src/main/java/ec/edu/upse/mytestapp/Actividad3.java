71445d65f63e14812b0d1aaf54b47d533ea66eda
Ivan <ivansanchez_1988@hotmail.com>
2016-10-22 14:48:15 -0500

    Practica de Layouts Terminada. No hay nada en los activities de los Layout Actividad 1, 2, 3, 4.

---
+++ app/src/main/java/ec/edu/upse/mytestapp/Actividad3.java
@@ -0,0 +1,13 @@
+package ec.edu.upse.mytestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Actividad3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad3);
+    }
+}
